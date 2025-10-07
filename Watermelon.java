5f0e0c2404ad51f4b4a64b0ea2c194e5d86c0cff
Keerthi RV <112474975+keerthicoder07@users.noreply.github.com>
2025-10-07 08:30:02 +0530

    Add files via upload

---
+++ Watermelon.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Watermelon
+{
+	public static void main(String[] args) {
+		int n;
+		int n1,n2;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n%2!=0 || n<=2)
+		{
+		    System.out.println("NO");
+		    return;
+		}
+		else
+		{
+		   System.out.println("YES");
+		          
+		}
+	}
+}

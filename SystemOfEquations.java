5db67f241dfaed81fc6dbaa6b47bf1e74a9dea50
Keerthi RV <112474975+keerthicoder07@users.noreply.github.com>
2025-10-14 11:07:36 +0530

    Update SystemOfEquations.java

--- SystemOfEquations.java
+++ SystemOfEquations.java
@@ -1,22 +1,15 @@
 import java.util.*;
-public class Main
+public class SystemOfEquations
 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int m=sc.nextInt();
-		int ma=n>m?n:m;
 		int mi=n<m?n:m;
 		int c=0;
-		if(m==1 && n==1)
+		for(int i=0;i<=mi;i++)
 		{
-		    System.out.println(2);
-		    return;
-		    
-		}
-		for(int i=0;i<=ma/2 && i<=mi;i++)
-		{
-		    for(int j=0;j<=ma/2 && j<=mi;j++)
+		    for(int j=0;j<=mi && j<=mi;j++)
 		    {
 		        if((Math.pow(i,2)+j)==n)
 		        {
